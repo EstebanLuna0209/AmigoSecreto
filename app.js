2bc789fbc850eb45f96ab1660c117e55d94cc45a
Esteban <jorge_e_luna@yahoo.com>
2025-02-25 11:38:25 -0600

    pimer actualizacion

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []
+
+function agregarAmigo() {
+    let nombreDeAmigos = document.getElementById('amigo');
+    if (nombreDeAmigos === '') {
+        alert('Introduce un nombre valido');
+    } else {
+        amigos.push(nombreDeAmigos.value);
+    }
+
+    console.log(amigos);
+    nombreDeAmigos.value = ''
+}
