345e33e3bf7576f9a8f2f316ae1f0e70aed451aa
Esteban <jorge_e_luna@yahoo.com>
2025-02-26 14:05:43 -0600

    Actualizacion 26-02-25

--- app.js
+++ app.js
@@ -2,14 +2,34 @@
 
 let amigos = []
 
-function agregarAmigo() {
-    let nombreDeAmigos = document.getElementById('amigo');
-    if (nombreDeAmigos === '') {
-        alert('Introduce un nombre valido');
-    } else {
-        amigos.push(nombreDeAmigos.value);
-    }
+// Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+// 
 
-    console.log(amigos);
-    nombreDeAmigos.value = ''
+function agregarAmigo() {
+    let capturaNombre = document.getElementById('amigo').value;
+
+// Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."//
+
+    if (capturaNombre === '') {
+        alert('Por favor ingresa un nombre');
+
+//Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().//
+
+    } else {
+        amigos.push(capturaNombre);
+        limpiarInput()
+    }
+}
+
+//Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.//
+
+function limpiarInput() {
+    document.getElementById('amigo').value = '';
+}
+
+//Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.//
+
+function lista() {
+    let lista = document.getElementById('listaAmigos').value;
+    
 }
