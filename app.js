723b026ae37dd2f143a3c044b5b86fbad07f279a
Esteban <jorge_e_luna@yahoo.com>
2025-03-06 12:11:39 -0600

    Actualizacion 060325 final

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
 
-let amigos = []
-
-// Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
-// 
 
 function agregarAmigo() {
-    let capturaNombre = document.getElementById('amigo').value;
-
-// Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."//
-
-    if (capturaNombre === '') {
+    let inputAmigo = document.getElementById('amigo');
+    let nombreCapturado = inputAmigo.value;
+    
+    if (!nombreCapturado) {
         alert('Por favor ingresa un nombre');
-
-//Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().//
-
+        return;
     } else {
-        amigos.push(capturaNombre);
-        limpiarInput()
+        amigo.push(nombreCapturado);
+        mostrarAmigos();
+        limpiarInput();
     }
 }
 
-//Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.//
+let inputAmigo = document.getElementById('amigo');
+inputAmigo.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        agregarAmigo();
+        limpiarInput();
+    }
+});
 
 function limpiarInput() {
-    document.getElementById('amigo').value = '';
+    inputAmigo.value = '';
+    inputAmigo.focus();
 }
 
-//Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.//
+function mostrarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    for (let i = 0; i < amigo.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+}
 
-function lista() {
-    let lista = document.getElementById('listaAmigos').value;
-    
+function sortearAmigo() {
+    if (amigo.length === 0) {
+        alert('No hay amigos para sortear');
+        return;
+    } else {
+        let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+        let resultadoSorteado = document.getElementById('resultado');
+        resultadoSorteado.innerHTML = `${amigoSorteado} es el amigo sorteado`;
+    }
+
+    let limpiarLista = document.getElementById('listaAmigos');
+    limpiarLista.innerHTML = '';
 }
